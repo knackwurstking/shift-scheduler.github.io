19359c13f2c31eea6f5a8b0c14a8c7e01cdf6dec
Udo Bauer <knackwurstking.tux@gmail.com>
2025-02-25 04:25:53 +0100

    feat: first build for github pages (dev version)

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/shift-scheduler.github.io/sw.js', { scope: '/shift-scheduler.github.io/' })})}
